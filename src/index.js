b0771c4ca4d47f807826b2e5380ed7c25004148b
Wesley <wes.beck21@outlook.com>
2021-03-27 09:48:48 -0700

    commit tree 1a295a0edc0dca5195406ee3157466fe401ab753

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
-ReactDOM.render(
-  <Home />,
+
+
+ReactDOM.render((
+  < Router >
+  <div>
+    <Navbar />
+    <Route path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/login" component={Login} />
+  </div>
+  </Router>),
   document.getElementById('root')
 );
